0ae5d954e9d884466b4edbe0a208f94591d49d22
Iryna <ipelipenko27@gmail.com>
2017-11-29 23:30:35 +0200

    second task

---
+++ task2.js
@@ -0,0 +1,7 @@
+var season=prompt("яка зараз пора року?","");
+(season=="Зима" || season=="зима") ? alert("Зима")
+:(season=="Весна" || season=="весна") ? alert("Весна")
+:(season=="Літо" || season=="літо") ? alert("Літо")
+:(season=="Осінь" || season=="осінь") ? alert("Осінь")
+:                     alert("Осінь")
+
